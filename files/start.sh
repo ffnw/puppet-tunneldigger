b05db1ab439cf81b956d825aa67f881791f5b2e9
Simon Kurka <dev@simon.kurka.cc>
2016-04-03 22:53:55 +0200

    venv activate needs to be sourced

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-/srv/tunneldigger/env_tunneldigger/bin/activate && \
+#!/bin/bash
+source /srv/tunneldigger/env_tunneldigger/bin/activate && \
 /srv/tunneldigger/env_tunneldigger/bin/python /srv/tunneldigger/tunneldigger/broker/broker.py "/etc/tunneldigger/$1/broker.cfg"
 
