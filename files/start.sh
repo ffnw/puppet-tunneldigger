2580664b5593af6b1911928c1e9a962452d68f4a
Simon Kurka <dev@simon.kurka.cc>
2018-01-05 17:01:45 +0100

    new startscript for new tunneldigger

--- files/start.sh
+++ files/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd /srv/tunneldigger/tunneldigger && \
-source /srv/tunneldigger/env_tunneldigger/bin/activate && \
-/srv/tunneldigger/env_tunneldigger/bin/python /srv/tunneldigger/tunneldigger/broker/l2tp_broker.py "/etc/tunneldigger/$1/broker.cfg"
-
+cd /srv/tunneldigger && \
+/srv/env_tunneldigger/bin/python -m tunneldigger_broker.main "/etc/tunneldigger/$1/broker.cfg"
